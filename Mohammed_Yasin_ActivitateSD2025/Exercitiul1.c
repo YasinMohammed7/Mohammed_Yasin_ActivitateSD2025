1c99f5d238807f5bf3858f8e2e0ab5c29c8f1672
Yasin Mohammed <mohammedyasinym16720@gmail.com>
2025-03-17 17:44:14 +0200

    commit 7

--- Mohammed_Yasin_ActivitateSD2025/Exercitiul1.c
+++ Mohammed_Yasin_ActivitateSD2025/Exercitiul1.c
@@ -1,175 +1,175 @@
-#include<stdio.h>
-#include<stdlib.h>
-
-struct Masina {
-	int id;
-	int caiPutere;
-	char* marca;
-	float capacitateMotor;
-	char model;
-};
-
-struct Masina initializare(int id, int caiPutere, const char* marca, float capacitateMotor, char model) {
-	struct Masina m;
-	m.id = id;
-	m.caiPutere = caiPutere;
-	m.capacitateMotor = capacitateMotor;
-	m.model = model;
-	m.marca = (char*)malloc(sizeof(char) * (strlen(marca) + 1));
-	strcpy_s(m.marca, strlen(marca) + 1, marca);
-	return m;
-}
-
-void afisare(struct Masina m) {
-	if (m.marca != NULL)
-		printf("%d. Masina cu marca %s are %d cai putere, %.2f capacitate motor si modelul %c\n", m.id, m.marca,m.caiPutere, m.capacitateMotor, m.model);
-	else 
-		printf("%d. Masina are %d cai putere, %.2f capacitate motor si modelul %c\n", m.id, m.caiPutere, m.capacitateMotor, m.model);
-
-}
-
-//void modificaCaiPutere(struct Masina* m, int caiPutere) {
-//	if (caiPutere > 0)
+//#include<stdio.h>
+//#include<stdlib.h>
+//
+//struct Masina {
+//	int id;
+//	int caiPutere;
+//	char* marca;
+//	float capacitateMotor;
+//	char model;
+//};
+//
+//struct Masina initializare(int id, int caiPutere, const char* marca, float capacitateMotor, char model) {
+//	struct Masina m;
+//	m.id = id;
+//	m.caiPutere = caiPutere;
+//	m.capacitateMotor = capacitateMotor;
+//	m.model = model;
+//	m.marca = (char*)malloc(sizeof(char) * (strlen(marca) + 1));
+//	strcpy_s(m.marca, strlen(marca) + 1, marca);
+//	return m;
+//}
+//
+//void afisare(struct Masina m) {
+//	if (m.marca != NULL)
+//		printf("%d. Masina cu marca %s are %d cai putere, %.2f capacitate motor si modelul %c\n", m.id, m.marca,m.caiPutere, m.capacitateMotor, m.model);
+//	else 
+//		printf("%d. Masina are %d cai putere, %.2f capacitate motor si modelul %c\n", m.id, m.caiPutere, m.capacitateMotor, m.model);
+//
+//}
+//
+////void modificaCaiPutere(struct Masina* m, int caiPutere) {
+////	if (caiPutere > 0)
+////	{
+////		m->caiPutere = caiPutere;
+////	}
+////}
+////
+////void dezalocare(struct Masina* m) {
+////	if (m->marca != NULL) {
+////		free(m->marca);
+////		m->marca = NULL;
+////	}
+////}
+//
+//void afisareVector(struct Masina* vector, int nrElemente) {
+//	for (int i = 0; i < nrElemente; i++)
 //	{
-//		m->caiPutere = caiPutere;
+//		afisare(vector[i]);
 //	}
 //}
 //
-//void dezalocare(struct Masina* m) {
-//	if (m->marca != NULL) {
-//		free(m->marca);
-//		m->marca = NULL;
+//struct Masina* copiazaPrimeleNElemente(struct Masina* vector, int nrElemente, int nrElementeCopiate) {
+//	
+//	struct Masina *vectorNou=NULL;
+//
+//	vectorNou = (struct Masina*)malloc(sizeof(struct Masina) * nrElementeCopiate);
+//
+//	for (int i = 0; i < nrElementeCopiate; i++) {
+//		vectorNou[i] = vector[i];
+//		vectorNou[i].marca = (char*)malloc(sizeof(char) * (strlen(vector[i].marca)+1));
+//		strcpy_s(vectorNou[i].marca, strlen(vector[i].marca) + 1, vector[i].marca);
+//	}
+//
+//	return vectorNou;
+//}
+//
+//void dezalocare(struct Masina** vector, int* nrElemente) {
+//	for (int i = 0; i < (*nrElemente); i++)
+//	{
+//		if ((*vector)[i].marca != NULL) {
+//			free((*vector)[i].marca);
+//		}
+//	}
+//
+//	free(*vector);
+//	*vector = NULL;
+//	*nrElemente = 0;
+//}
+//
+//void copiazaMasiniPuternice(struct Masina* vector, char nrElemente, float capacitateMinima, struct Masina** vectorNou, int* dimensiune) {
+//	*dimensiune = 0;
+//
+//	for (int i = 0; i < nrElemente; i++)
+//	{
+//		if (vector[i].capacitateMotor > capacitateMinima) {
+//			(*dimensiune)++;
+//		}
+//	}
+//
+//	if ((*vectorNou) != NULL) {
+//		free(*vectorNou);
+//	}
+//
+//	int k = 0;
+//
+//	*vectorNou = (struct Masina*)malloc(sizeof(struct Masina) * (*dimensiune));
+//	for (int i = 0; i < nrElemente; i++)
+//	{
+//		if (vector[i].capacitateMotor > capacitateMinima)
+//		{
+//			(*vectorNou)[k] = vector[i];
+//			(*vectorNou)[k].marca = (char*)malloc(sizeof(char) * (strlen(vector[i].marca) + 1));
+//			strcpy_s((*vectorNou)[k].marca, strlen(vector[i].marca) + 1, vector[i].marca);
+//			k++;
+//		}
 //	}
 //}
-
-void afisareVector(struct Masina* vector, int nrElemente) {
-	for (int i = 0; i < nrElemente; i++)
-	{
-		afisare(vector[i]);
-	}
-}
-
-struct Masina* copiazaPrimeleNElemente(struct Masina* vector, int nrElemente, int nrElementeCopiate) {
-	
-	struct Masina *vectorNou=NULL;
-
-	vectorNou = (struct Masina*)malloc(sizeof(struct Masina) * nrElementeCopiate);
-
-	for (int i = 0; i < nrElementeCopiate; i++) {
-		vectorNou[i] = vector[i];
-		vectorNou[i].marca = (char*)malloc(sizeof(char) * (strlen(vector[i].marca)+1));
-		strcpy_s(vectorNou[i].marca, strlen(vector[i].marca) + 1, vector[i].marca);
-	}
-
-	return vectorNou;
-}
-
-void dezalocare(struct Masina** vector, int* nrElemente) {
-	for (int i = 0; i < (*nrElemente); i++)
-	{
-		if ((*vector)[i].marca != NULL) {
-			free((*vector)[i].marca);
-		}
-	}
-
-	free(*vector);
-	*vector = NULL;
-	*nrElemente = 0;
-}
-
-void copiazaMasiniPuternice(struct Masina* vector, char nrElemente, float capacitateMinima, struct Masina** vectorNou, int* dimensiune) {
-	*dimensiune = 0;
-
-	for (int i = 0; i < nrElemente; i++)
-	{
-		if (vector[i].capacitateMotor > capacitateMinima) {
-			(*dimensiune)++;
-		}
-	}
-
-	if ((*vectorNou) != NULL) {
-		free(*vectorNou);
-	}
-
-	int k = 0;
-
-	*vectorNou = (struct Masina*)malloc(sizeof(struct Masina) * (*dimensiune));
-	for (int i = 0; i < nrElemente; i++)
-	{
-		if (vector[i].capacitateMotor > capacitateMinima)
-		{
-			(*vectorNou)[k] = vector[i];
-			(*vectorNou)[k].marca = (char*)malloc(sizeof(char) * (strlen(vector[i].marca) + 1));
-			strcpy_s((*vectorNou)[k].marca, strlen(vector[i].marca) + 1, vector[i].marca);
-			k++;
-		}
-	}
-}
-
-struct Masina getPrimaMasinabyMarca(struct Masina* vector, int nrElemente, const char* marca) {
-	
-	struct Masina m;
-	m.marca = NULL;
-	for (int i = 0; i < nrElemente; i++)
-	{
-		if (strcmp(vector[i].marca, marca) == 0) {
-			m = vector[i];
-			m.marca = (char*)malloc(strlen(vector[i].marca) + 1);
-			strcpy_s(m.marca, strlen(vector[i].marca) + 1, vector[i].marca);
-			return m;
-		}
-	}
-
-	return m;
-}
-
-int main() {
-	//struct Masina m;
-
-	//m = initializare(1, 100, "logan", 1.0, 'A');
-
-	//modificaCaiPutere(&m, 120);
-
-	//afisare(m);
-	//dezalocare(&m);
-
-	struct Masina* masini = NULL;
-	int nrMasini = 3;
-	masini = (struct Masina*)malloc(sizeof(struct Masina) * nrMasini);
-
-	masini[0] = initializare(1, 100, "Tesla", 2.2, 'S');
-	masini[1] = initializare(2, 140, "BMW", 2.6, 'C');
-	masini[2] = initializare(3, 190, "Maseratti", 3.6, 'T');
-
-	afisareVector(masini, nrMasini);
-
-	struct Masina* primeleMasini = NULL;
-	int nrPrimeleMasini = 2;
-
-	primeleMasini = copiazaPrimeleNElemente(masini, nrMasini, nrPrimeleMasini);
-
-	afisareVector(primeleMasini, nrPrimeleMasini);
-
-	dezalocare(&primeleMasini, &nrPrimeleMasini);
-	afisareVector(primeleMasini, nrPrimeleMasini);
-
-	struct Masina* masiniPuternice = NULL;
-	int nrMasiniPuternice = 0;
-	copiazaMasiniPuternice(masini, nrMasini, 2.0, &masiniPuternice, &nrMasiniPuternice);
-	printf("\n\nMasini puternice:\n");
-	afisareVector(masiniPuternice, nrMasiniPuternice);
-
-	dezalocare(&masiniPuternice, &nrMasiniPuternice);
-	printf("\n\nPrima masina:\n");
-	struct Masina primaMasina = getPrimaMasinabyMarca(masini, nrMasini, "Maseratti");
-	afisare(primaMasina);
-
-	if (primaMasina.marca != NULL)
-	{
-		free(primaMasina.marca);
-		primaMasina.marca = NULL;
-	}
-
-	dezalocare(&masini, &nrMasini);
-	return 0;
-}
+//
+//struct Masina getPrimaMasinabyMarca(struct Masina* vector, int nrElemente, const char* marca) {
+//	
+//	struct Masina m;
+//	m.marca = NULL;
+//	for (int i = 0; i < nrElemente; i++)
+//	{
+//		if (strcmp(vector[i].marca, marca) == 0) {
+//			m = vector[i];
+//			m.marca = (char*)malloc(strlen(vector[i].marca) + 1);
+//			strcpy_s(m.marca, strlen(vector[i].marca) + 1, vector[i].marca);
+//			return m;
+//		}
+//	}
+//
+//	return m;
+//}
+//
+//int main() {
+//	//struct Masina m;
+//
+//	//m = initializare(1, 100, "logan", 1.0, 'A');
+//
+//	//modificaCaiPutere(&m, 120);
+//
+//	//afisare(m);
+//	//dezalocare(&m);
+//
+//	struct Masina* masini = NULL;
+//	int nrMasini = 3;
+//	masini = (struct Masina*)malloc(sizeof(struct Masina) * nrMasini);
+//
+//	masini[0] = initializare(1, 100, "Tesla", 2.2, 'S');
+//	masini[1] = initializare(2, 140, "BMW", 2.6, 'C');
+//	masini[2] = initializare(3, 190, "Maseratti", 3.6, 'T');
+//
+//	afisareVector(masini, nrMasini);
+//
+//	struct Masina* primeleMasini = NULL;
+//	int nrPrimeleMasini = 2;
+//
+//	primeleMasini = copiazaPrimeleNElemente(masini, nrMasini, nrPrimeleMasini);
+//
+//	afisareVector(primeleMasini, nrPrimeleMasini);
+//
+//	dezalocare(&primeleMasini, &nrPrimeleMasini);
+//	afisareVector(primeleMasini, nrPrimeleMasini);
+//
+//	struct Masina* masiniPuternice = NULL;
+//	int nrMasiniPuternice = 0;
+//	copiazaMasiniPuternice(masini, nrMasini, 2.0, &masiniPuternice, &nrMasiniPuternice);
+//	printf("\n\nMasini puternice:\n");
+//	afisareVector(masiniPuternice, nrMasiniPuternice);
+//
+//	dezalocare(&masiniPuternice, &nrMasiniPuternice);
+//	printf("\n\nPrima masina:\n");
+//	struct Masina primaMasina = getPrimaMasinabyMarca(masini, nrMasini, "Maseratti");
+//	afisare(primaMasina);
+//
+//	if (primaMasina.marca != NULL)
+//	{
+//		free(primaMasina.marca);
+//		primaMasina.marca = NULL;
+//	}
+//
+//	dezalocare(&masini, &nrMasini);
+//	return 0;
+//}
---
+++ Mohammed_Yasin_ActivitateSD2025/Exercitiul2.c
@@ -0,0 +1,55 @@
+#define CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+//trebuie sa folositi fisierul masini.txt
+//sau va creati un alt fisier cu alte date
+
+struct StructuraStilou {
+	int id;
+	int lungime;
+	float pret;
+	char* firma;
+	char* culoare;
+	unsigned char model;
+};
+typedef struct StructuraStilou Stilou;
+
+void afisareStilou(Stilou stilou) {
+	printf("ID: %d", stilou.id);
+	printf("Lungime: %d", stilou.lungime);
+	printf("Pret: %.1f", stilou.pret);
+	printf("Firma: %s", stilou.firma);
+	printf("culoare: %s", stilou.culoare);
+	printf("Model: %c", stilou.model);
+}
+
+void afisareVectorStilouri(Stilou* stilouri, int nrStilouri) {
+}
+
+void adaugaStilouInVector(Stilou** stilouri, int* nrStilouri, Stilou stilouNoua) {
+	//adauga in vectorul primit o noua Stilou pe care o primim ca parametru
+	//ATENTIE - se modifica numarul de Stilouri din vector;
+}
+
+Stilou citireStilouFisier(FILE* file) {
+	//functia citeste o Stilou dintr-un strceam deja deschis
+	//Stilou citita este returnata;
+}
+
+Stilou* citireVectorStilouriFisier(const char* numeFisier, int* nrStilouriCitite) {
+	//functia primeste numele fisierului, il deschide si citeste toate Stilourile din fisier
+	//prin apelul repetat al functiei citireStilouFisier()
+	//numarul de Stilouri este determinat prin numarul de citiri din fisier
+	//ATENTIE - la final inchidem fisierul/stream-ul
+}
+
+void dezalocareVectorStilouri(Stilou** vector, int* nrStilouri) {
+	//este dezalocat intreg vectorul de masini
+}
+
+int main() {
+	
+	return 0;
+}
